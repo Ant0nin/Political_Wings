49eec7f0741aa951e3dacf2b93f73e624c514592
Antonin Bernardin <antonin.bernardin@gmail.com>
2016-05-30 22:51:36 +0200

    Ajout d'un petit script pour interpoler la transformation d'un objet vers une autre transform

---
+++ Assets/GameContent/Scripts/TransformOnClick.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TransformOnClick : MonoBehaviour
+{
+    public Camera mainCamera;
+    public GameObject nextLocation;
+    public float speed = 0.3F;
+
+    private float startTime;
+    private bool trigger = false;
+
+    private Vector3 startLocationPos;
+    private Quaternion startLocationRotate;
+    private Transform endLocation;
+
+	void Start () {
+        startLocationPos = transform.position;
+        startLocationRotate = transform.rotation;
+        endLocation = nextLocation.transform;
+        startTime = Time.time;
+    }
+
+    void OnMouseOver() {
+        if (Input.GetMouseButtonDown(0))
+            trigger = !trigger;
+    }
+
+    void Update()
+    {
+        if (trigger) {
+            transform.position = Vector3.Lerp(transform.position, endLocation.position, speed);
+            transform.rotation = Quaternion.Lerp(transform.rotation, endLocation.rotation, speed);
+        }
+        else
+        {
+            transform.position = Vector3.Lerp(transform.position, startLocationPos, speed);
+            transform.rotation = Quaternion.Lerp(transform.rotation, startLocationRotate, speed);
+        }
+    }
+}
