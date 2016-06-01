8053c799e6824ba2ead1166b63fb8f6727d64bf1
Antonin Bernardin <antonin.bernardin@gmail.com>
2016-06-01 14:34:56 +0200

    Mise en place de la 'main scene'

---
+++ Assets/GameContent/Scripts/ChangeSceneOnClick.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ChangeSceneOnClick : MonoBehaviour {
+
+    public float fadeSpeed = 1.5f;
+    public string nextSceneName;
+    public Color fadeOutColor = Color.white;
+
+    private GUITexture screenTexture;
+    private bool goToTargetScene;
+
+    void Awake()
+    {
+        screenTexture = GetComponent<GUITexture>();
+        screenTexture.pixelInset = new Rect(0f, 0f, Screen.width, Screen.height);
+        screenTexture.color = fadeOutColor;
+    }
+
+    void OnMouseOver()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            goToTargetScene = true;
+        }
+    }
+
+    void Update()
+    {
+        if (goToTargetScene)
+            NextScene();
+    }
+
+    void FadeToColor()
+    {
+        screenTexture.color = Color.Lerp(screenTexture.color, fadeOutColor, fadeSpeed * Time.deltaTime);
+    }
+
+    void NextScene()
+    {
+        screenTexture.enabled = true;
+        FadeToColor();
+
+        if (screenTexture.color.a >= 0.95f)
+        {
+            SceneManager.LoadScene(nextSceneName);
+        }
+    }
+}
---
+++ Assets/GameContent/Scripts/InitKeysDisplay.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InitKeysDisplay : MonoBehaviour {
+
+	void Start () {
+
+        PlayerProgress progressComp = GameObject.Find("PlayerProgress").GetComponent<PlayerProgress>();
+
+        GameObject keyPuzzleDomino = transform.GetChild(0).gameObject;
+        GameObject keyPuzzleBoule = transform.GetChild(1).gameObject;
+        GameObject keyPuzzleLaser = transform.GetChild(2).gameObject;
+        GameObject keyPuzzleSimon = transform.GetChild(3).gameObject;
+
+        if (!progressComp.winPuzzleDomino)
+            keyPuzzleDomino.SetActive(false);
+
+        if (!progressComp.winPuzzleBoule)
+            keyPuzzleBoule.SetActive(false);
+
+        if (!progressComp.winPuzzleLaser)
+            keyPuzzleLaser.SetActive(false);
+
+        if (!progressComp.winPuzzleSimon)
+            keyPuzzleSimon.SetActive(false);
+
+        if (progressComp.winPuzzleDomino && progressComp.winPuzzleBoule && progressComp.winPuzzleLaser && progressComp.winPuzzleSimon)
+            OpenCoffre();
+    }
+
+    void OpenCoffre()
+    {
+        // TODO
+    }
+}
